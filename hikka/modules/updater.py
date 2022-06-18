2744600cf9547452d73c374a552809b79d207ade
Onne Wona <104727779+onnewona@users.noreply.github.com>
2022-06-18 11:31:46 +0500

    Update updater.py

--- hikka/modules/updater.py
+++ hikka/modules/updater.py
@@ -117,7 +117,7 @@ class UpdaterMod(loader.Module):
         self.config = loader.ModuleConfig(
             loader.ConfigValue(
                 "GIT_ORIGIN_URL",
-                "https://github.com/hikariatama/Hikka",
+                "https://github.com/onnewona/onne",
                 lambda: self.strings("origin_cfg_doc"),
                 validator=loader.validators.Link(),
             )
